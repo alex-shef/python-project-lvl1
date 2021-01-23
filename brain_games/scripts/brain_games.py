46cff10169eacab4927b7d65fc80224aacf6af04
alex-shef <alexei.shef@gmail.com>
2021-01-24 02:41:53 +0300

    step 3 - Third-party library

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
+from brain_games.cli import welcome_user
+
+
 def greet():
     print('Welcome to the Brain Games!')
 
+
 def main():
     greet()
+    welcome_user()
+
 
 if __name__ == '__main__':
     main()
